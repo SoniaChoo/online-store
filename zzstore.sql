8484b8f9640c9318478736c82d754b518d12d81b
Sonia <soniachoo.zhu@gmail.com>
2020-02-07 00:08:02 +0800

    undo fmt

--- zzstore.sql
+++ zzstore.sql
@@ -19,21 +19,20 @@ SET FOREIGN_KEY_CHECKS=0;
 -- Table structure for dish
 -- ----------------------------
 DROP TABLE IF EXISTS `dish`;
-CREATE TABLE `dish`
-(
-    `dish_id`     int(11)      NOT NULL AUTO_INCREMENT,
-    `rest_id`     int(11)               DEFAULT NULL,
-    `price`       float        NOT NULL,
-    `dish_name`   varchar(255) NOT NULL,
-    `description` varchar(255) NOT NULL,
-    `stock`       int(11)      NOT NULL,
-    `sales`       int(11)      NOT NULL,
-    `favorite`    int(11)      NOT NULL,
-    `creat_time`  timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `update_time` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`dish_id`),
-    KEY `rest_id` (`rest_id`),
-    CONSTRAINT `dish_ibfk_1` FOREIGN KEY (`rest_id`) REFERENCES `rest` (`rest_id`) ON DELETE SET NULL ON UPDATE CASCADE
+CREATE TABLE `dish` (
+  `dish_id` int(11) NOT NULL AUTO_INCREMENT,
+  `rest_id` int(11) DEFAULT NULL,
+  `price` float NOT NULL,
+  `dish_name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `sales` int(11) NOT NULL,
+  `favorite` int(11) NOT NULL,
+  `creat_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`dish_id`),
+  KEY `rest_id` (`rest_id`),
+  CONSTRAINT `dish_ibfk_1` FOREIGN KEY (`rest_id`) REFERENCES `rest` (`rest_id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -60,65 +59,61 @@ CREATE TABLE `order_detail` (
 -- Table structure for orders
 -- ----------------------------
 DROP TABLE IF EXISTS `orders`;
-CREATE TABLE `orders`
-(
-    `order_id`    int(11)   NOT NULL AUTO_INCREMENT,
-    `user_id`     int(11)            DEFAULT NULL,
-    `status`      int(11)   NOT NULL,
-    `price`       float     NOT NULL,
-    `creat_time`  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`order_id`),
-    KEY `user_id` (`user_id`),
-    CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `status` int(11) NOT NULL,
+  `price` float NOT NULL,
+  `creat_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`order_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for picture
 -- ----------------------------
 DROP TABLE IF EXISTS `picture`;
-CREATE TABLE `picture`
-(
-    `pic_id`      int(11)      NOT NULL AUTO_INCREMENT,
-    `types`       int(11)      NOT NULL,
-    `pic_path`    varchar(255) NOT NULL,
-    `first_pic`   int(11)      NOT NULL,
-    `creat_time`  timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `update_time` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`pic_id`)
+CREATE TABLE `picture` (
+  `pic_id` int(11) NOT NULL AUTO_INCREMENT,
+  `types` int(11) NOT NULL,
+  `pic_path` varchar(255) NOT NULL,
+  `first_pic` int(11) NOT NULL,
+  `creat_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`pic_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for rest
 -- ----------------------------
 DROP TABLE IF EXISTS `rest`;
-CREATE TABLE `rest`
-(
-    `rest_id`     int(11)      NOT NULL AUTO_INCREMENT,
-    `user_id`     int(11)               DEFAULT NULL,
-    `phone`       varchar(255) NOT NULL,
-    `rest_name`   varchar(255) NOT NULL,
-    `address`     varchar(255) NOT NULL,
-    `create_time` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `update_time` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`rest_id`),
-    UNIQUE KEY `phone` (`phone`),
-    KEY `user_id` (`user_id`),
-    CONSTRAINT `rest_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+CREATE TABLE `rest` (
+  `rest_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `phone` varchar(255) NOT NULL,
+  `rest_name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`rest_id`),
+  UNIQUE KEY `phone` (`phone`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `rest_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`
-(
-    `user_id`     int(11)      NOT NULL AUTO_INCREMENT,
-    `phone`       varchar(255) NOT NULL,
-    `nickname`    varchar(255) NOT NULL,
-    `password`    varchar(255) NOT NULL,
-    `creat_time`  timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `update_time` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`user_id`),
-    UNIQUE KEY `phone` (`phone`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `phone` varchar(255) NOT NULL,
+  `nickname` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `creat_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `phone` (`phone`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
