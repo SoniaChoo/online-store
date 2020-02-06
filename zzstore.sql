d3794f9bd311f8fa1b01b52bcb1b6f2f6162d2f0
Sonia <soniachoo.zhu@gmail.com>
2020-02-06 23:46:45 +0800

    update database schema

--- zzstore.sql
+++ zzstore.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : hk_aliyun
+Source Server         : HK
 Source Server Version : 50727
 Source Host           : hk.ziyitony.cn:3306
 Source Database       : zzstore
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50727
 File Encoding         : 65001
 
-Date: 2020-01-12 20:09:21
+Date: 2020-02-06 23:37:41
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -19,17 +19,21 @@ SET FOREIGN_KEY_CHECKS=0;
 -- Table structure for dish
 -- ----------------------------
 DROP TABLE IF EXISTS `dish`;
-CREATE TABLE `dish` (
-  `dish_id` int(11) NOT NULL AUTO_INCREMENT,
-  `rest_id` int(11) DEFAULT NULL,
-  `price` float NOT NULL,
-  `dish_name` varchar(255) NOT NULL,
-  `stock` int(11) NOT NULL,
-  `sales` int(11) NOT NULL,
-  `creat_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`dish_id`),
-  KEY `rest_id` (`rest_id`),
-  CONSTRAINT `dish_ibfk_1` FOREIGN KEY (`rest_id`) REFERENCES `rest` (`rest_id`) ON DELETE SET NULL ON UPDATE CASCADE
+CREATE TABLE `dish`
+(
+    `dish_id`     int(11)      NOT NULL AUTO_INCREMENT,
+    `rest_id`     int(11)               DEFAULT NULL,
+    `price`       float        NOT NULL,
+    `dish_name`   varchar(255) NOT NULL,
+    `description` varchar(255) NOT NULL,
+    `stock`       int(11)      NOT NULL,
+    `sales`       int(11)      NOT NULL,
+    `favorite`    int(11)      NOT NULL,
+    `creat_time`  timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`dish_id`),
+    KEY `rest_id` (`rest_id`),
+    CONSTRAINT `dish_ibfk_1` FOREIGN KEY (`rest_id`) REFERENCES `rest` (`rest_id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -56,55 +60,65 @@ CREATE TABLE `order_detail` (
 -- Table structure for orders
 -- ----------------------------
 DROP TABLE IF EXISTS `orders`;
-CREATE TABLE `orders` (
-  `order_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `status` int(11) NOT NULL,
-  `price` float DEFAULT NULL,
-  `creat_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`order_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE
+CREATE TABLE `orders`
+(
+    `order_id`    int(11)   NOT NULL AUTO_INCREMENT,
+    `user_id`     int(11)            DEFAULT NULL,
+    `status`      int(11)   NOT NULL,
+    `price`       float     NOT NULL,
+    `creat_time`  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`order_id`),
+    KEY `user_id` (`user_id`),
+    CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for picture
 -- ----------------------------
 DROP TABLE IF EXISTS `picture`;
-CREATE TABLE `picture` (
-  `pic_id` int(11) NOT NULL AUTO_INCREMENT,
-  `types` int(11) NOT NULL,
-  `pic_path` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-  `first_pic` int(11) NOT NULL,
-  `creat_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`pic_id`)
+CREATE TABLE `picture`
+(
+    `pic_id`      int(11)      NOT NULL AUTO_INCREMENT,
+    `types`       int(11)      NOT NULL,
+    `pic_path`    varchar(255) NOT NULL,
+    `first_pic`   int(11)      NOT NULL,
+    `creat_time`  timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`pic_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for rest
 -- ----------------------------
 DROP TABLE IF EXISTS `rest`;
-CREATE TABLE `rest` (
-  `rest_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `phone` varchar(255) DEFAULT NULL,
-  `rest_name` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`rest_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `rest_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE
+CREATE TABLE `rest`
+(
+    `rest_id`     int(11)      NOT NULL AUTO_INCREMENT,
+    `user_id`     int(11)               DEFAULT NULL,
+    `phone`       varchar(255) NOT NULL,
+    `rest_name`   varchar(255) NOT NULL,
+    `address`     varchar(255) NOT NULL,
+    `create_time` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`rest_id`),
+    UNIQUE KEY `phone` (`phone`),
+    KEY `user_id` (`user_id`),
+    CONSTRAINT `rest_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `phone` varchar(255) NOT NULL,
-  `nickname` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `creat_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_id`)
+CREATE TABLE `user`
+(
+    `user_id`     int(11)      NOT NULL AUTO_INCREMENT,
+    `phone`       varchar(255) NOT NULL,
+    `nickname`    varchar(255) NOT NULL,
+    `password`    varchar(255) NOT NULL,
+    `creat_time`  timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`user_id`),
+    UNIQUE KEY `phone` (`phone`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
