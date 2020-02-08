0e9f3ea7e417a9373a6e571376dbba13a6caa966
SoniaChoo <soniachoo.zhu@gmail.com>
2020-02-08 16:53:04 +0900

    Dish update api and unit test (#18)

--- http/dishHandler_test.go
+++ http/dishHandler_test.go
@@ -141,7 +141,91 @@ func TestAddHandlerDish(t *testing.T) {
 	if err != nil {
 		t.Fatal("read response body error")
 	}
-	if string(b) != fmt.Sprintf(SuccessfullyAddDish,"maocai") {
+	if string(b) != fmt.Sprintf(SuccessfullyAddDish, "maocai") {
 		t.Fatal("expect successfully add, got other")
+	}
+}
+
+func TestUpdateHandlerDishWithBadJSon(t *testing.T) {
+	body := strings.NewReader(``)
+	req, err := http.NewRequest(http.MethodPost, "/dish/update", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/dish/update", UpdateHandlerDish)
+	mux.ServeHTTP(w, req)
+
+	resp := w.Result()
+
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Fatal("expect 400, got other")
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal("read response body error")
+	}
+	if string(b) != BadJsonDish {
+		t.Fatal("expect json format error, got other")
+	}
+}
+
+func TestUpdateHandlerDishWithNotAvailable(t *testing.T) {
+	body := strings.NewReader(`{"price":0,"dishname":"maocai","description":"sichuan cai, is delicious","stock":10,"dishid":1}`)
+	req, err := http.NewRequest(http.MethodPost, "/dish/update", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/dish/update", UpdateHandlerDish)
+	mux.ServeHTTP(w, req)
+
+	resp := w.Result()
+
+	if resp.StatusCode != http.StatusInternalServerError {
+		t.Fatal("expect 500, got other")
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal("read response body error")
+	}
+	if string(b) != RequestUpdateNotAvailable {
+		t.Fatal("expect parameter not available error, got other")
+	}
+}
+
+func TestUpdateHandlerDish(t *testing.T) {
+	body := strings.NewReader(`{"price":888,"dishname":"maocai","description":"sichuan cai, is delicious","stock":10,"dishid":1}`)
+	req, err := http.NewRequest(http.MethodPost, "/dish/update", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/dish/update", UpdateHandlerDish)
+	mux.ServeHTTP(w, req)
+
+	resp := w.Result()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("expect 200, got other")
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal("read response body error")
+	}
+	if string(b) != fmt.Sprintf(SuccessfullyUpdateDish, 1) {
+		t.Fatal("expect successfully update dish, got other")
 	}
 }
